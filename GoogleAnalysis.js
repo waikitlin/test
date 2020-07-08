7e6a9018dbb52c36001396aaf29778affcb0dc92
Wai Kit <linwaikit@gmail.com>
2020-07-08 11:46:14 +0800

    Add files via upload

---
+++ GoogleAnalysis.js
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 <!-- Global site tag (gtag.js) - Google Analytics -->\
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-80480159-1"></script>\
+<script>\
+  window.dataLayer = window.dataLayer || [];\
+  function gtag()\{dataLayer.push(arguments);\}\
+  gtag('js', new Date());\
+\
+  gtag('config', 'UA-80480159-1');\
+</script>\
+}
